a16003ec8ac2367c9b9deadc5c7349d11a485b08
raghav1010 <raghavkol1997@gmail.com>
2024-01-14 20:11:33 +0530

    configured black for linting codebase

--- manage.py
+++ manage.py
@@ -9,9 +9,11 @@ def main():
     """Run administrative tasks."""
 
     if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfecommerce.settings.local')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drfecommerce.settings.local")
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfecommerce.settings.production')
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "drfecommerce.settings.production"
+        )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -23,5 +25,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
